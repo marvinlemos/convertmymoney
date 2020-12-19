f01d581b061ef4f50864b895d3c4f77db90d8812
Marvin Lemos <marvinlemos@gmail.com>
2020-12-19 14:06:48 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Convert My Money
+This a small project to show some basic features of Node and Express framework. 
+
+Convert My Money allows Brazilian users to convert US Dollar to Brazilian Real.
+
+## Begining
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Requirements:
+
+You need to have NodeJs and NPM installed.
+
+### Running the project:
+
+```
+npm install
+node index.js
+```
+
+## Licença
+MIT - [LICENSE.md](LICENSE.md).
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const path = require('path')
+const convert = require('./lib/convert')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/exchange', (req, res) =>{
+    const {rate, amount} = req.query
+    if (rate && amount){
+        const converted = convert.convert(rate, amount)
+        res.render('exchange', {
+            error: false,
+            rate: convert.toMoney(rate),
+            amount: convert.toMoney(amount),
+            converted: convert.toMoney(converted)
+        })
+    }else{
+        res.render('exchange',{
+            error: 'invalid values'
+        })
+    }
+})
+
+app.listen(3000, err => {
+    if (err){
+        console.log(err)
+    }else{
+        console.log('ConvertMyMoney is running...')
+    }
+})
